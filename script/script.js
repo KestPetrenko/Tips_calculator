a05d347359d737f3645e6c1fdcd50f51a426e3cc
Konstantin Petrenko <109843396+KestPetrenko@users.noreply.github.com>
2022-09-11 12:07:33 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -14,26 +14,26 @@ let sum;
 
 
 function proc5() {
-    Math.ceil(((Number(input1.value) * 5) / 100 * Number(input2.value)))
+    Math.ceil(((Number(input1.value) * 5) / 100 * Number(input2.value)));
     res.textContent = "Сумма на каждого по: " + sum + " грн";
     showTime();
     console.log(res.textContent)
 }
 
 function proc10() {
-    Math.ceil(((Number(input1.value) * 10) / 100 * Number(input2.value)))
+    Math.ceil(((Number(input1.value) * 10) / 100 * Number(input2.value)));
     res.textContent = "Сумма на каждого по: " + sum + " грн";
     showTime();
 }
 
 function proc15() {
-    Math.ceil(((Number(input1.value) * 15) / 100 * Number(input2.value)))
+    Math.ceil(((Number(input1.value) * 15) / 100 * Number(input2.value)));
     res.textContent = "Сумма на каждого по: " + sum + " грн";
     showTime();
 }
 
 function proc20() {
-    Math.ceil(((Number(input1.value) * 20) / 100 * Number(input2.value)))
+    Math.ceil(((Number(input1.value) * 20) / 100 * Number(input2.value)));
     res.textContent = "Сумма на каждого по: " + sum + " грн";
     showTime();
 }
